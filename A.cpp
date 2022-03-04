bfe103f6acdd822a9af82ca747d4a0d37eaa1d68
Srikanth Iyengar <ksrikanth3012@gmail.com>
2022-03-04 14:00:15 +0530

    first file

---
+++ A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Obstacle {
+	vector<pair<int, int>> points;
+	Obstacle() {
+		for(int i = 0 ; i < 3 ; i++) {
+			int x, y ; cin >> x >> y;
+			points.push_back({x, y});
+		}
+	}
+};
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+	int xs, ys, xf, yf; cin >> xs >> ys >> xf >> yf;
+	int n; cin >> n;
+	vector<Obstacle> v;
+	for(int i = 0 ; i < n ; i++) {
+		v.push_back(Obstacle());
+	}
+}
